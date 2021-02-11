dd280b50f93de3e2d126a0405a27c206b568e022
daschaich <daschaich@gmail.com>
2021-02-11 02:28:56 +0000

    Minor python improvements

--- computer_project/demo.py
+++ computer_project/demo.py
@@ -8,19 +8,30 @@ import matplotlib.pyplot as plt
 random.seed(42)
 nSamples = 100000
 dat = []
+vev = 0.0
+vevSq = 0.0
 for i in range(0, nSamples):
   dat.append(2.0 * math.sqrt(random.random()))
+  vev += dat[-1]
+  vevSq += dat[-1] * dat[-1]
+
+vev /= nSamples
+vevSq /= nSamples
+print("mu =", vev)
+print("sigma =", math.sqrt(vevSq - vev*vev))
 
 nbins = 51
 plt.hist(dat, nbins, density=True)
 x = np.arange(0, 2, 0.01)
 y = 0.5 * x
 plt.plot(x, y)
-plt.savefig('hist.png')
+plt.show()
+#plt.savefig('hist.png')
 
 # Compute walk lengths
 N_list = [10, 20, 30, 40, 50, 60, 70, 80]
 L = []
+print("N  L")
 for Nstep in N_list:
   L.append(0.0)
   for i in range(0, nSamples):
@@ -29,23 +40,39 @@ for Nstep in N_list:
       d += 2.0 * math.sqrt(random.random())
     L[-1] += d
   L[-1] /= float(nSamples)
+  print(Nstep, L[-1])
 
 # Check that walk lengths look reasonable
 plt.clf()
 plt.plot(N_list, L, linestyle='None', marker=".")
 
-# Fit walk lengths to straight line
-out = np.polyfit(N_list, L, 1)
-print(out)
-x = np.arange(0, 80, 0.1)
-p = out[0] * x + out[1]
-plt.plot(x, p)
-plt.savefig('fit.png')
+# Fit walk lengths to straight line, L = a * N + b
+output = np.polyfit(N_list, L, 1)
+a=output[0]
+b=output[1]
+print("a =", a)
+print("b =", b)
 
-# Fit walk lengths to generalized power law, L = D * x^alpha
-# Expect linear power, alpha=1 --> L ~ x
-# TRICK: ln(L) = alpha * ln(x) + ln(D) is now linear
+x = np.arange(0, 80, 0.1)
+p = a * x + b
+plt.plot(x, p)
+plt.show()
+#plt.savefig('fit.png')
+
+# Fit walk lengths to generalized power law, L = c * N^alpha
+# Expect linear power, alpha=1 --> L ~ N
+# TRICK: log(L) = alpha * log(N) + log(c) is now linear
 logN = np.log(N_list);
 logL = np.log(L);
-out = np.polyfit(logN, logL, 1)
-print(out)
+output = np.polyfit(logN, logL, 1)
+alpha = output[0]
+c=np.exp(output[1])
+print("alpha =", alpha)
+print("c =", c)
+
+plt.clf()
+plt.plot(N_list, L, linestyle='None', marker=".")
+x = np.arange(0, 80, 0.1)
+p = c * x**alpha
+plt.plot(x, p)
+plt.show()
